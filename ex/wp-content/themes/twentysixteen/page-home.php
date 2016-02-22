09fabe0fae1076c63f936a6730b41c5baf3f81b9
Su Excalibur <suexcalibur@gmail.com>
2016-02-22 14:32:20 +0600

    Starting of the project
    Configured WordPress added & tested files to see if its working.

---
+++ ex/wp-content/themes/twentysixteen/page-home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Caohagan Home
+ */
+?>
+
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
+  </head>
+
+  <body>
+    <div>Working</div>
+  </body>
+
+  </html>
